7c611fd0e020542996ce944611744b6b9f851d91
Júlio César Cesário <julioccesar988@gmail.com>
2024-11-19 18:56:39 -0300

    add dependencies

--- Agendei-mobile/App.js
+++ Agendei-mobile/App.js
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import Login from "./src/screens/login/login.jsx";
+import Account from "./src/screens/account/account.jsx";
+import AbaHome from "./src/screens/abahome/abahome.jsx";
+import AbaCalendar from "./src/screens/abaCalendar/abacalendar.jsx";
+import AbaProfile from "./src/screens/abaProfile/abaProfile.jsx";
+import Main from "./src/screens/main/main.jsx";
+import Sevices from "./src/screens/services/services.jsx";
+import Schedule from "./src/screens/schedule/schedule.jsx";
 
 function App() {
   return (
-    <View>
-      <Text>Jornada js</Text>
-    </View>
+    <>
+      <Main />
+    </>
   );
 }
 
