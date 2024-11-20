e535e40713a70eab9d3f1c02e69138e468217edb
Júlio César Cesário <julioccesar988@gmail.com>
2024-11-19 21:11:21 -0300

    add API dependencies

--- Agendei-API/src/index.js
+++ Agendei-API/src/index.js
@@ -1,17 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes.js';
+import express from "express";
+import cors from "cors";
+import router from "./routes.js";
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
-app.use(router)
-
-
-
+app.use(router);
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}.!`);
-    
-})
+  console.log(`Servidor rodando na porta ${port}.!`);
+});
